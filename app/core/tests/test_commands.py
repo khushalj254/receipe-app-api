0aaa60f99f9ef68fd87624259545bd886afc0d5c
khushal <khushalj254@gmail.com>
2022-10-14 14:58:32 +0530

    adding wait for db command1

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -13,7 +13,7 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandsTest(SimpleTestCase):
-    """Test commands."""
+    """Test commands. test"""
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
